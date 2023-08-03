dda56d60588eaa87f3f6d6ca6a31d98dc36f0b5d
Muhammad Junaid <121503655+junaidsalam639@users.noreply.github.com>
2023-08-03 13:28:51 +0500

    Add files via upload
    Post App (html , css , Javascript , Bootstrap and firebase);

---
+++ app.js
@@ -0,0 +1,119 @@
+import { app, db, storage, auth } from './firebase.mjs'
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+import { collection, query, where, getDocs, doc, deleteDoc, updateDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+import { getDownloadURL, ref, deleteObject, uploadBytes } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-storage.js";
+
+
+
+const querySnapshot = await getDocs(collection(db, "cardAdd"));
+querySnapshot.forEach((doc) => {
+    console.log(doc.id, '=>', doc.data());
+
+    var url1;
+    getDownloadURL(ref(storage, doc.id))
+        .then((url) => {
+
+            url1 = url
+
+            let add = document.getElementById('card-add-img').innerHTML += `
+                <div class="col mt-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="${url1}" class="card-img-top" alt="" id="img">
+                        <div class="card-body">
+                          <h5 class="card-title" id="title">Title: ${doc.data().text}</h5><hr>
+                          <h6 class="card-text" id="desc">Desc: ${doc.data().text1}</h6><hr>
+                          <h6 class="card-text" id="desc">Price: ${doc.data().price}</h6><hr>
+                          <div class="btn">
+                          <button onclick="edit('${doc.id}')" class='edit' data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button>
+                          <button onclick="dele('${doc.id}')" class='dele'>Delete</button>
+                          </div>
+                          </div>
+                          </div>
+                          </div>`
+        })
+        .catch((error) => {
+            // Handle any errors
+        });
+});
+
+// Delete code
+async function dele(e) {
+    console.log(e);
+    // Create a reference to the file to delete
+    const desertRef = ref(storage, e);
+
+    // Delete the file
+    deleteObject(desertRef).then(() => {
+
+    }).catch((error) => {
+        console.log(error);
+    });
+
+    await deleteDoc(doc(db, "cardAdd", e));
+    Swal.fire({
+        icon: 'success',
+        title: 'Deleted!',
+        text: 'Your file has been deleted.',
+    }).then(() => {
+        location.reload();
+    })
+}
+window.dele = dele
+
+
+
+
+// Edit code
+async function edit(e) {
+    console.log(e);
+    document.getElementById('btn-change').addEventListener('click', async () => {
+        let title1 = document.getElementById('title1');
+        let desc1 = document.getElementById('desc1');
+        let price1 = document.getElementById('price1');
+        
+
+        if (title1.value == '' || desc1.value == '' || price1.value == '' || image1.value == '') {
+            Swal.fire({
+                icon: 'error',
+                title: 'Input Fill',
+                text: 'Please Fill The Input!',
+            })
+        } else {
+            let image1 = document.getElementById('image1').files[0];
+            console.log(image1);
+            console.log(title1.value);
+            console.log(desc1.value);
+            console.log(price1.value);
+
+
+
+            const washingtonRef = doc(db, "cardAdd", e);
+            // Set the "capital" field of the city 'DC'
+            await updateDoc(washingtonRef, {
+                text: title1.value,
+                text1: desc1.value,
+                price: price1.value,
+            });
+
+            const storageRef = ref(storage, e);
+
+            // 'file' comes from the Blob or File API
+            uploadBytes(storageRef, image1).then((snapshot) => {
+                console.log('Uploaded a blob or file!');
+            });
+            Swal.fire({
+                icon: 'success',
+                title: 'Edit',
+                text: 'Edit Successfully!',
+            })
+                .then(() => {
+                    location.reload();
+                })
+        }
+    })
+}
+window.edit = edit
+
+
+
+
